fba3bc90f4677cf9724a29dc855cb05a70e94268
Mitingi Joy <mitingijoychesang@gmail.com>
2023-03-22 07:42:00 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,38 @@
+//strings length()
+//returns the number of characters in string.
+const one ="I love rain";
+console.log(one.length);
+//string replace()
+//replace a new string with the specified string
+const two = "I love rain"
+console.log(two.replace("I love rain","very much"))
+//string indexOf()
+//returns the index of the first character of the substring in a string
+const three ="I love rain";
+console.log(three.indexOf("rain"))
+//lastIndexOf()
+//returns the last index of occurence of a given substring in the string
+
+//startsWith()
+//returns true if a string is a string begins with specified characters
+
+//endsWith()
+//returns true if a string ends with a specified strings
+
+//strings toUpperCase()
+//returns a string converted to upperCase
+const six = "i love rain"
+console.log(six.toUpperCase())
+//strings to lowerCase()
+//returns the converted to small letters
+const seven = "i love rain"
+console.log(seven.toLowerCase())
+//includes()
+//checks if a string can be found in another string
+const eight = "i love rain"
+console.log(eight.includes("rain"))
+//repeat()
+//creates a new strings by repeating the given specified string
+//charAt() 
+//returns the character at the specified index in a string
+//javascript
